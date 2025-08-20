d7cc3f5488ccfa2316afa372518e58f490921453
tolgahandayanikli <tolgahan.dayanikli@gmail.com>
2025-08-20 16:06:09 +0200

    sikecem artik

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import Hero from "@/components/landing/hero/Hero";
 import Cta from "@/components/landing/cta/Cta";
 import Navbar from "@/components/layout/Navbar/Navbar";
-import Footer from "@/components/layout/Navbar/Footer";
+import Footer from "@/components/layout/Footer/Footer";
 export default function Home() {
   return (
     <>
