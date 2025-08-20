b3fc8131360ea0d19292983ed2854653aa9b27a3
tolgahandayanikli <tolgahan.dayanikli@gmail.com>
2025-08-20 15:57:00 +0200

    sikecem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Navbar from "@/components/layout/navbar/Navbar";
+
 import Hero from "@/components/landing/hero/Hero";
 import Cta from "@/components/landing/cta/Cta";
-import Footer from "@/components/layout/footer/Footer";
-
+import Navbar from "@/components/layout/navbar/Navbar";
+import Footer from "@/components/layout/navbar/Footer";
 export default function Home() {
   return (
     <>
